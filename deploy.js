172ea72d9f8d61326045fb34cfa380e24f4ec80e
Joen Sindholt <joensindholt@gmail.com>
2020-01-22 21:07:32 +0100

    Enable auto deploy

--- deploy.js
+++ deploy.js
@@ -1,23 +1,20 @@
 var FtpDeploy = require("ftp-deploy");
 var ftpDeploy = new FtpDeploy();
- 
+
 var config = {
-    user: process.env.DEPLOY_FTP_USERNAME,
-    password: process.env.DEPLOY_FTP_PASSWORD,
-    host: "ftp.gentofte-ik.dk",
-    port: 21,
-    localRoot: __dirname + "/wordpress/gik-theme",
-    remoteRoot: "/public_html/.../",
-    include: ["*", "**/*"],
-    exclude: ["*.ts"],
-    deleteRemote: false,
-    forcePasv: true
+  user: process.env.DEPLOY_FTP_USERNAME,
+  password: process.env.DEPLOY_FTP_PASSWORD,
+  host: "ftp.gentofte-ik.dk",
+  port: 21,
+  localRoot: __dirname + "/wordpress/gik-theme",
+  remoteRoot: "/wp-content/themes/gik-theme",
+  include: ["*", "**/*"],
+  exclude: ["*.ts"],
+  deleteRemote: false,
+  forcePasv: true
 };
 
-console.log(config);
- 
-// use with promises
-// ftpDeploy
-//     .deploy(config)
-//     .then(res => console.log("finished:", res))
-//     .catch(err => console.log(err));
+ftpDeploy
+  .deploy(config)
+  .then(res => console.log("finished:", res))
+  .catch(err => console.log(err));
